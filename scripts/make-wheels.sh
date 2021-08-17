0aa7621ba524c94d7b0d1e783d8e74fcd58de5a1
Dmitry Orlov <me@mosquito.su>
2021-08-18 00:12:28 +0300

    add python3.10 and pypy wheels

--- scripts/make-wheels.sh
+++ scripts/make-wheels.sh
@@ -1,7 +1,7 @@
 set -ex
 
 mkdir -p dist
-make -j4
+make
 
 function build_wheel() {
   /opt/python/$1/bin/pip install cython
@@ -12,6 +12,8 @@ build_wheel cp36-cp36m
 build_wheel cp37-cp37m
 build_wheel cp38-cp38
 build_wheel cp39-cp39
+build_wheel cp310-cp310
+build_wheel pp37-pypy37_pp73
 
 cd dist
 for f in ./*linux_*;
